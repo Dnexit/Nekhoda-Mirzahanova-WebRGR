e1d98fff3a3501555f6a3384f4ade1a4f8fa9ed0
IvanKot <wocker@i.ua>
2023-06-16 20:23:11 +0300

    Сделал редактор текста TinyMCE

--- resources/views/reviews.blade.php
+++ resources/views/reviews.blade.php
@@ -23,7 +23,11 @@
                                 <h4 class="nav-masthead my-0 font-weight-normal">{{$d['timestamp']}}</h4>
                             </div>
                             <div class="card-body bg-dark">
-                                <p class="masthead-brand text-lg-start">{{$d['text']}}</p>
+                                <p class="masthead-brand text-lg-start">
+                                    @php
+                                        {{echo htmlspecialchars_decode($d['text']);}}
+                                    @endphp
+                                </p>
                                 @if(session('isModer') == 1 | session('isAdmin') == 1)
                                     <div class="nav-masthead">
                                         <a href="/reviews_editor/{{$d['id']}}"><img src="images/edit.png" width="30" height="30"></a>
@@ -48,7 +52,22 @@
                         <div class="card mb-4 box-shadow">
                             <div class="card-header bg-dark">
                                 <div class="container">
-                                    <textarea class="form-control mt-3" id="reviewtext" name="reviewtext"></textarea>
+                                    <textarea class="form-control mt-3" id="reviewtext" name="reviewtext">
+     Welcome to TinyMCE!
+  </textarea>
+                                    <script>
+                                        tinymce.init({
+                                            selector: 'textarea',
+                                            plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss',
+                                            toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+                                            tinycomments_mode: 'embedded',
+                                            tinycomments_author: 'Author name',
+                                            mergetags_list: [
+                                                { value: 'First.Name', title: 'First Name' },
+                                                { value: 'Email', title: 'Email' },
+                                            ]
+                                        });
+                                    </script>
                                 </div>
                                 <button type="submit" href="/main" class="btn btn-lg btn-block btn-light m-3">Оставить отзыв</button>
                             </div>
